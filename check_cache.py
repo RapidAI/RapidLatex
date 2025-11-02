e4f51c07bc667c16e31b178ffc444790b9e4f2cf
znsoft <znsoft@163.com>
2025-11-02 10:15:28 +0800

    update file.

--- check_cache.py
+++
@@ -1,8 +0,0 @@
-import appdata
-import os
-paths = appdata.AppDataPaths('mathtranslate')
-cache_dir = os.path.join(paths.app_data_path, 'cache')
-print('Cache dir:', cache_dir)
-print('Exists:', os.path.exists(cache_dir))
-if os.path.exists(cache_dir):
-    print('Contents:', os.listdir(cache_dir))
